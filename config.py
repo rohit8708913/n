3f4ab1c861e04e13a180f094f0e55cf36c519f28
rohit3607 <rohitplayer87089@gmail.com>
2024-10-06 15:18:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store privat
 #your telegram tag without @
 OWNER_TAG = os.environ.get("OWNER_TAG", "rohit_1888")
 #Time in seconds for message delete, put 0 to never delete
-TIME = int(os.environ.get("TIME", "600"))
+TIME = int(os.environ.get("TIME", "60"))
 
 
 #Shortner (token system) 
